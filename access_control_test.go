65f47153e7026eabb04006bbb6a568ed0341b33e
ThomasDao <thomas.daoxuandung@gmail.com>
2015-11-12 15:09:25 +0800

    Test travis

--- access_control_test.go
+++ access_control_test.go
@@ -1,8 +1,11 @@
 package goal_test
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
+	"net/http/httptest"
+	"testing"
 
 	"github.com/thomasdao/goal"
 )
@@ -27,54 +30,64 @@ func (art *article) Query(w http.ResponseWriter, request *http.Request) (int, in
 	return goal.HandleQuery(art, request)
 }
 
-// func TestCanRead(t *testing.T) {
-// 	setup()
-// 	defer tearDown()
-//
-// 	// Create article with author
-// 	author := &testuser{}
-// 	author.Username = "secret"
-// 	db.Create(author)
-//
-// 	art := &article{}
-// 	art.Author = author
-// 	art.Permission = &goal.Permission{
-// 		Read:  `["admin", "ceo"]`,
-// 		Write: `["admin", "ceo"]`,
-// 	}
-// 	art.Title = "Top Secret"
-//
-// 	err := db.Create(art).Error
-// 	if err != nil {
-// 		fmt.Println("error create article ", err)
-// 	}
-//
-// 	res := httptest.NewRecorder()
-//
-// 	var json = []byte(`{"username":"thomasdao", "password": "something-secret"}`)
-// 	req, _ := http.NewRequest("POST", "/auth/register", bytes.NewBuffer(json))
-//
-// 	goal.SharedAPI().Mux().ServeHTTP(res, req)
-//
-// 	// Make sure cookies is set properly
-// 	hdr := res.Header()
-// 	cookies, ok := hdr["Set-Cookie"]
-// 	if !ok || len(cookies) != 1 {
-// 		t.Fatal("No cookies. Header:", hdr)
-// 	}
-//
-// 	artURL := fmt.Sprint(server.URL, "/article/", art.ID)
-//
-// 	// Make sure user is the same with current user from session
-// 	nextReq, _ := http.NewRequest("GET", artURL, nil)
-// 	nextReq.Header.Add("Cookie", cookies[0])
-//
-// 	// Get response
-// 	client := &http.Client{}
-// 	resp, err := client.Do(nextReq)
-// 	resp.Body.Close()
-//
-// 	if resp.StatusCode != 403 || err != nil {
-// 		t.Error("Request should be unauthorized because thomasdao doesn't have admin role")
-// 	}
-// }
+func TestCanRead(t *testing.T) {
+	setup()
+	defer tearDown()
+
+	// Create article with author
+	author := &testuser{}
+	author.Username = "secret"
+	db.Create(author)
+
+	fmt.Println("Create author")
+
+	art := &article{}
+	art.Author = author
+	art.Permission = &goal.Permission{
+		Read:  `["admin", "ceo"]`,
+		Write: `["admin", "ceo"]`,
+	}
+	art.Title = "Top Secret"
+
+	err := db.Create(art).Error
+	fmt.Println("Create article")
+	if err != nil {
+		fmt.Println("error create article ", err)
+	}
+
+	res := httptest.NewRecorder()
+
+	var json = []byte(`{"username":"thomasdao", "password": "something-secret"}`)
+	req, _ := http.NewRequest("POST", "/auth/register", bytes.NewBuffer(json))
+
+	fmt.Println("Register thomasdao")
+
+	goal.SharedAPI().Mux().ServeHTTP(res, req)
+
+	// Make sure cookies is set properly
+	hdr := res.Header()
+	cookies, ok := hdr["Set-Cookie"]
+	if !ok || len(cookies) != 1 {
+		t.Fatal("No cookies. Header:", hdr)
+	}
+
+	fmt.Println("register success")
+	artURL := fmt.Sprint(server.URL, "/article/", art.ID)
+
+	fmt.Println("Before request artURL ", artURL)
+
+	// Make sure user is the same with current user from session
+	nextReq, _ := http.NewRequest("GET", artURL, nil)
+	nextReq.Header.Add("Cookie", cookies[0])
+
+	// Get response
+	client := &http.Client{}
+	resp, err := client.Do(nextReq)
+	resp.Body.Close()
+
+	fmt.Println("After request artURL")
+
+	if resp.StatusCode != 403 || err != nil {
+		t.Error("Request should be unauthorized because thomasdao doesn't have admin role")
+	}
+}
